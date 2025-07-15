f9cae5837243385005a4bbeaa0cd9a886e0a8232
Carlos Tapia <ctapiadev@gmail.com>
2025-07-15 02:51:39 -0400

    deploy vercel

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -2,6 +2,9 @@
 
 import { createClient } from '@supabase/supabase-js'
 
+console.log("URL RECIBIDA:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY RECIBIDA:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 // Obtenemos las variables de entorno
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
